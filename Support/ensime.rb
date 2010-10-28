56c5c1c4d376392982d5c4bbabab590e0784b2db
Mads Hartmann <mads379@gmail.com>
2010-10-28 12:37:44 +0200

    The server now logs it's message in the html output so you can see what is going on.

--- Support/ensime.rb
+++ Support/ensime.rb
@@ -1,4 +1,13 @@
+SUPPORT_LIB = ENV['TM_SUPPORT_PATH'] + '/lib/'
+
 require "socket"
+require SUPPORT_LIB + 'io'
+require SUPPORT_LIB + 'tm/htmloutput'
+require SUPPORT_LIB + 'tm/process'
+# require SUPPORT_LIB + 'tm/require_cmd'
+# require SUPPORT_LIB + 'escape'
+# require SUPPORT_LIB + 'exit_codes'
+
 
 module Ensime
   
@@ -19,13 +28,15 @@ module Ensime
     
     def initialize_project
       msg = "(:swank-rpc (swank:connection-info) 1)"
-      full = encode_message(msg)
+      full = create_message(msg)
       @socket.print(full)
       tmp = @socket.recv(128)
       # TODO: what to do with the answer?
     end
     
-    def encode_message(msg)
+    # creates a message that the ensime backend can read. This is done
+    # by prepending the length of the message (hex-encoded).
+    def create_message(msg)
       size = msg.length.to_s(16) # 16 bit 
       header = size.to_s.length.upto(MESSAGE_HEADER_SIZE-1).collect{0}
       header = header + size.split('')
@@ -60,17 +71,31 @@ module Ensime
     
     # Start the server. 
     def start
-      port = pick_port
-      server = TCPServer.open(port)   
-      loop {  # Servers run forever                        
-        Thread.start(server.accept) do |client|
-          msg = client.recv(128)
-          @socket.print(msg)
-          response = @socket.recv(128)
-          client.print(response)   # Send the time to the client
-          client.close            # Disconnect from the client
-        end
-      }
+      
+      TextMate::HTMLOutput.show(
+        :title => "Textmate ENSIME Server", 
+        :sub_title => "Logs", 
+        :html_head => script_style_header) do |io|
+        
+        
+        io << "<pre></code>"
+
+        port = pick_port
+        server = TCPServer.open(port)   
+        loop {  # Servers run forever                        
+          Thread.start(server.accept) do |client|
+            msg = client.recv(128)
+            io << "<p>Forwarding message:\n#{msg}\n</p>"
+            @socket.print(msg)
+            response = @socket.recv(128)
+            client.print(response)    # Send the time to the client
+            client.close              # Disconnect from the client
+          end
+        }
+        
+        io << "<code></pre>"
+        
+      end  
     end
       
     # Write the port this server is going to use to the file
@@ -97,7 +122,99 @@ module Ensime
       return TCPSocket.open("127.0.0.1", port)      
     end
     
+    def script_style_header
+      return <<-HTML
+  <!-- executor javascripts -->
+  <script type="text/javascript" charset="utf-8">
+  function press(evt) {
+   if (evt.keyCode == 67 && evt.ctrlKey == true) {
+     TextMate.system("kill -s USR1 #{::Process.pid};", null);
+   }
+  }
+
+  function copyOutput(element) {
+  output = element.innerText;
+  cmd = TextMate.system('__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 /usr/bin/pbcopy', function(){});
+  cmd.write(output);
+  cmd.close();
+  element.innerText = 'output copied to clipboard';
+  }
+
+  </script>
+  <!-- end javascript -->
+  <style type="text/css">
+
+  div.executor .controls {
+    text-align:right;
+    float:right;
+  }
+  div.executor .controls a {
+    text-decoration: none;
+  }
+
+  div.executor pre em
+  {
+    font-style: normal;
+    color: #FF5600;
+  }
+
+  div.executor p#exception strong
+  {
+    color: #E4450B;
+  }
+
+  div.executor p#traceback
+  {
+    font-size: 8pt;
+  }
+
+  div.executor blockquote {
+    font-style: normal;
+    border: none;
+  }
+
+  div.executor table {
+    margin: 0;
+    padding: 0;
+  }
+
+  div.executor td {
+    margin: 0;
+    padding: 2px 2px 2px 5px;
+    font-size: 10pt;
+  }
+
+  div.executor div#_executor_output {
+    white-space: normal;
+    -khtml-nbsp-mode: space;
+    -khtml-line-break: after-white-space;
+  }
+
+  div#_executor_output .out {  
+
+  }
+  div#_executor_output .err {  
+    color: red;
+  }
+  div#_executor_output .echo {
+    font-style: italic;
+  }
+  div#_executor_output .test {
+    font-weight: bold;
+  }
+  div#_executor_output .test.ok {  
+    color: green;
+  }
+  div#_executor_output .test.fail {  
+    color: red;
+  }
+  div#exception_report pre.snippet {
+    margin:4pt;
+    padding:4pt;
+  }
+  </style>
+  HTML
+    end
+    
   end
-  
-  
 end
